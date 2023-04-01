94e14459d6d477eb0e071a755902ffc489c6674b
Alin RADU <alin.radu.cristian@gmail.com>
2023-04-01 09:13:32 +0300

    add fetchData

---
+++ 8-react-advanced/fetchData/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function App() {
+  const [user, setUser] = React.useState([]);
+  console.log('%c-> developmentConsole: user= ', 'color:#77dcfd', user);
+
+  const URL = 'https://randomuser.me/api/?results=1';
+  const fetchData = () => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setUser(data.results[0]));
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div style={{ padding: '40px' }}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.name.first}</h2>
+      <img src={user.picture.large} alt="User Picture" />
+    </div>
+  ) : (
+    <h1>Data pending...</h1>
+  );
+}
+
+export default App;
