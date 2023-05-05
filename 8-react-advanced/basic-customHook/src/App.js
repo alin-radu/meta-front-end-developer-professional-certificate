936ec0e38af9a5665fa7510e2e2c725186666c53
Alin RADU <alin.radu.cristian@gmail.com>
2023-05-05 10:14:15 +0300

    investigate the implementation of basic-customHook

--- 8-react-advanced/basic-customHook/src/App.js
+++ 8-react-advanced/basic-customHook/src/App.js
@@ -1,8 +1,23 @@
 import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
+  console.log(
+    '%c-> developmentConsole: App --------------------------------------> ',
+    'color:#77dcfd'
+  );
+
   const [day, setDay] = useState('Monday');
+
+  console.log('%c-> developmentConsole: App | day= ', 'color:#77dcfd', day);
+
   const prevDay = usePrevious(day);
+
+  useEffect(() => {
+    console.log('%c-> developmentConsole: App | useEffect= ENTERED ', 'color:#77dcfd');
+  }, []);
+
+  console.log('%c-> developmentConsole: App | prevDay= ', 'color:#77dcfd', prevDay);
+
   const getNextDay = () => {
     if (day === 'Monday') {
       setDay('Tuesday');
@@ -16,6 +31,9 @@ export default function App() {
       setDay('Monday');
     }
   };
+
+  console.log('%c-> developmentConsole: App | BEFORE RETURN ', 'color:#77dcfd');
+
   return (
     <div style={{ padding: '40px' }}>
       <h1>
@@ -28,14 +46,30 @@ export default function App() {
   );
 }
 function usePrevious(val) {
-  console.log('%c-> developmentConsole: val= ', 'color:#77dcfd', val);
+  console.log(
+    '%c-> developmentConsole: usePrevious --------------------------------------> ',
+    'color:#77dcfd'
+  );
+
+  console.log('%c-> developmentConsole: usePrevious | val= ', 'color:#77dcfd', val);
 
   const ref = useRef();
   useEffect(() => {
+    console.log(
+      '%c-> developmentConsole: usePrevious | useEffect= ENTERED ',
+      'color:#77dcfd'
+    );
+
     ref.current = val;
   }, [val]);
 
-  console.log('%c-> developmentConsole: ref.current= ', 'color:#77dcfd', ref.current);
+  console.log(
+    '%c-> developmentConsole: usePrevious | ref.current= ',
+    'color:#77dcfd',
+    ref.current
+  );
+
+  console.log('%c-> developmentConsole: usePrevious | BEFORE RETURN ', 'color:#77dcfd');
 
   return ref.current;
 }
